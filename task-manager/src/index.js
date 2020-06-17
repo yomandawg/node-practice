3767c92595c73618cc75560a971e471a1c447cc4
Ray Kim <yomandawgshit@gmail.com>
2020-06-18 01:15:14 +0900

    multer

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,10 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000 // Heroku dependent PORT
 
-// app.use((req, res, next) => {
-//   res.status(503).send('Site is currently down. Check back soon!')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
-const { update } = require('../models/user')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -167,6 +167,25 @@ router.delete('/users/me', auth, async (req, res) => {
   } catch(e) {
     res.status(500).send()
   }
+})
+
+const upload = multer({
+  dest: 'avatars',
+  limits: {
+    fileSize: 1000000 // 1MB
+  },
+  fileFilter(req, file, cb) {
+    if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image.'))
+    }
+    cb(undefined, true)
+  }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), (req, res) => {
+  res.send()
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message })
 })
 
 module.exports = router
